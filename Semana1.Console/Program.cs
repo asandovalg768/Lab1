5bc4b98febac3422082de6dd254b47429f873140
asandovalg768 <43359591+asandovalg768@users.noreply.github.com>
2018-09-18 16:04:49 -0600

    Add files via upload

---
+++ Semana1.Console/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana1.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Persona");
+
+        
+
+
+            Persona persona = new Persona();
+            persona.Id = 1;
+            persona.Nombre = "Alejandro";
+            persona.Apellido = "Sandoval";
+            ILogger logger = persona;
+
+
+          
+            string personString = string.Format("Id:{0}, Nombre: {1}, Apellido: {2}", persona.Id, persona.Nombre, persona.Apellido);
+            System.Console.WriteLine(personString);
+            logger.Log("Hola Mundo");
+            System.Console.ReadKey();
+        }
+    }
+}
---
+++ Semana1/Persona.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana1
+{
+    public class Persona:ILogger
+    {
+        public Persona()
+        {
+        }
+
+        public Persona(string nombre, int id, string apellido)
+        {
+            Nombre = nombre;
+            Id = id;
+            Apellido = apellido;
+        }
+
+        public string Nombre { get; set; }
+        public int Id { get; set; }
+        public string Apellido { get; set; }
+
+        public void Log(string log)
+        {
+            System.Console.WriteLine("Usuario desea ingresar");
+        }
+    }
+}
