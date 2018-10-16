ee36ad9247d935b4511bdd0e16ddbd6357db5584
asandovalg768 <43359591+asandovalg768@users.noreply.github.com>
2018-10-16 02:48:56 -0600

    Update Publicacion.cs

--- Taller1Publicaciones/Taller1Publicaciones/Publicacion.cs
+++ Taller1Publicaciones/Taller1Publicaciones/Publicacion.cs
@@ -9,10 +9,10 @@ namespace Taller1Publicaciones
     public class Publicacion 
     {
 
-        public string texto { get; set; }
-        public DateTime fecha { get; set; }
-        public int idPublicacion { get; set; }
-        public string nombreUsuario { get; set; }
+        public string texto { set; get; }
+        public DateTime fecha { set; get; }
+        public int idPublicacion { set; get; }
+        public string nombreUsuario { set; get; }
 
     }
 }
